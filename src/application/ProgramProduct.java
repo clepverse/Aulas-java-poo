dcc18cd570856d1d1f9e567814b92f5cd67cbca5
Aldo Migge <cleepe5@hotmail.com>
2021-06-07 21:29:55 -0300

    Aprimorando codigo usando construtores e encapsulamento

--- src/application/ProgramProduct.java
+++ src/application/ProgramProduct.java
@@ -6,19 +6,19 @@ public class ProgramProduct {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Product product = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+        Product product = new Product(name, price, quantity);
         System.out.println();
         System.out.println("Product data: " + product);
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
         System.out.println();
         System.out.println("Updated data: " + product);
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -3,15 +3,29 @@ public class Product {
     public String name;
     public double price;
     public int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product() {
+
+    }
+
     public double totalValueInStock() {
         return price * quantity;
     }
+
     public void addProducts(int quantity) {
         this.quantity += quantity;
     }
+
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
+
     public String toString() {
         return name
                 + ", $ "
