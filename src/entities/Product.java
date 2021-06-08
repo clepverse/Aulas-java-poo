fc8e1b07ed4b3cc5bc07a6149834510c76cbf59a
Aldo Migge <cleepe5@hotmail.com>
2021-06-08 00:49:33 -0300

    Adicionando mais atividades de POO

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -4,14 +4,39 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product() {
+    }
+
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public Product() {
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        quantity = 0;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock() {
